ea412b6ff49a0b5b564f8dd10458f30ee5caa37b
Pranshu Chittora <pranshuchittora17@gmail.com>
2019-11-21 17:25:01 +0530

    version: v1.0.0
    - Upadates pjson

--- example/basic.js
+++ example/basic.js
@@ -1,5 +1,6 @@
 const gfactor = require("../dist/index").default;
 
+process.env.SIZE = '90000000000000000000'
 console.log(gfactor('./'));
 console.log(gfactor('../'));
 // console.log(gfactor('../../'));
--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ export default function(filePath) {
   process.env.SIZE = "0";
   getSize({ filelist: [] }, filePath);
 
-  const SIZE_String = process.env.SIZE;
-  const SIZE_Number = convertBytes(parseInt(SIZE_String));
-  return { SIZE_Number, SIZE_String };
+  const SIZE_Number = parseInt(process.env.SIZE);
+  const SIZE_Parsed = convertBytes(SIZE_Number);
+  return { SIZE_Parsed, SIZE_Number };
 }
